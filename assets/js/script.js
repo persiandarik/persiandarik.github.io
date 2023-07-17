35087a4dfa683e9e22dad1b379d7ff39ed41d36f
persiandarik <persiandarik@gmail.com>
2023-07-17 12:56:33 +0330

    'classContact'

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,3 +89,24 @@ const filterFunc = function (selectedValue) {
   }
 
 }
+
+
+
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input field
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+
+    // check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+
+  });
+}
